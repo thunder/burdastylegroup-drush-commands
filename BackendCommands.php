d82d64a5b1d69e7bb2b17c977c1ae3663fe96692
Daniel Bosen <daniel.bosen@burda.com>
2020-12-22 11:53:43 +0100

    Fix copy

--- BackendCommands.php
+++ BackendCommands.php
@@ -208,8 +208,9 @@ class BackendCommands extends DrushCommands implements SiteAliasManagerAwareInte
                     $modifiedFiles[$fileName] = $fullPath;
                 }
             } else {
-                $this->filesystem->copy($fullPath, $overrideFiles[$fileName]);
+                $this->filesystem->copy($fullPath, $this->siteConfigOverrideDirectory() . '/' . $fileName);
                 $this->io()->block('New configuration file "'.$fileName.'" was added to override folder. Please check, if that is the correct location.', 'INFO', 'fg=yellow');
+                $modifiedFiles[$fileName] = $fullPath;
             }
         }
 
