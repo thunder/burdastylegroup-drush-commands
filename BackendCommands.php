a1433e00a2ec015f0301db7737ba27d3c0f1e15e
Killesreiter Volker <killesre@gmail.com>
2024-08-27 11:06:57 +0200

    fix cs 4

--- BackendCommands.php
+++ BackendCommands.php
@@ -42,7 +42,7 @@ class BackendCommands extends DrushCommands implements SiteAliasManagerAwareInte
      * @hook init @options-backend
      *
      * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @param \Consolidation\AnnotatedCommand\AnnotationData $annotationData
+     * @param \Consolidation\AnnotatedCommand\AnnotationData  $annotationData
      */
     public function initCommands(InputInterface $input, AnnotationData $annotationData)
     {
